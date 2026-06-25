aa3638672614e0d0afa94f9075155a9a8408e551
Jules Munyaneza <mjules.tek@gmail.com>
2026-06-25 16:03:05 +0200

    Refactor buildApplication and clean up code
    Refactor buildApplication function to include proper syntax and remove unnecessary return statements.

--- script.groovy
+++ script.groovy
@@ -2,18 +2,19 @@ def buildJar() {
     echo 'Packaging the jar file...'
     sh 'mvn package'
 }
-return this
 
 def buildApplication() {
     echo "building the docker image..."
-    withCredentials ([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t mujuules01/demo-app:jma-2.0 ."
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push mujuules01/demo-app:jma-2.0'
-}                
-return this
+    }
+}
 
 def deployApplication() {
     echo 'deploying the application...'
-    }
+}
+
 return this
